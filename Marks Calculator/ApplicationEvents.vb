2bdf09249ba55094f81a80bb679b1ba04f7f0d76
Connie Lam <lamconnielamsiuyin@live.hk>
2024-07-21 13:48:37 +0800

    Add LICENSE texts.

--- Marks Calculator/ApplicationEvents.vb
+++ Marks Calculator/ApplicationEvents.vb
@@ -1,6 +1,22 @@
-﻿' Name:         Marks Calculator Project
-' Purpose:      Used to calculate student grades.
-' Programmer:   Chow Cheuk Hei, Tse Ka Yu on 2 / 10 / 2022
+﻿'   Marks Calculator Project
+'   Purpose: Used to calculate student grades.
+'   Copyright © 2022, Edmond Chow and its contributors, All rights reserved.
+'
+'   This program is free software; you can redistribute it and/or modify
+'   it under the terms of the GNU General Public License as published by
+'   the Free Software Foundation; either version 2 of the License, or
+'   (at your option) any later version.
+'
+'   This program is distributed in the hope that it will be useful,
+'   but WITHOUT ANY WARRANTY; without even the implied warranty of
+'   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+'   GNU General Public License for more details.
+'
+'   You should have received a copy of the GNU General Public License along
+'   with this program; if not, write to the Free Software Foundation, Inc.,
+'   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+'
+'   For more information, please contact 69935538chow@gmail.com.
 
 Imports Microsoft.VisualBasic.ApplicationServices
 
